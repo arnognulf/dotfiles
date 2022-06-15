999b053c16f105230a3ef922b0484f83cdea7b10
Thomas Eriksson <thomas.eriksson@gmail.com>
2022-06-15 21:31:28 +0200

    yoyo

--- deal-with-it/deal-with-it.sh
+++ deal-with-it/deal-with-it.sh
@@ -7,6 +7,11 @@ exit 0
 fi
 echo -n "$$" >~/.cache/deal-with-it.pid
 COUNT=0
+WALKCOUNT=0
+killAfter10Min ()
+{
+    sleep 600 && kill -9 $(command ps aux|command grep multi-actions.py|command grep -v grep|command awk '{ print $2 }';) &>/dev/null; 
+}
 while sleep 1;
 do
     if LC_ALL=C gnome-screensaver-command -q | grep "is active"
@@ -16,16 +21,24 @@ do
         let COUNT++
     fi
 
+    #if [ $WALKCOUNT -ge 2 ]
+    #then
+    #    ( killAfter10Min & )
+    #    python3 ~/.config/dotfiles/deal-with-it/multi-actions.py "WALK LIKE AN EGYPTIAN" "𓀛𓅱𓃝"
+    #    WALKCOUNT=0
+    #elif [ $COUNT -ge 1200 ]
     if [ $COUNT -ge 1200 ]
     then
         if type -p notify-send.py &>/dev/null
         then
             notify-send.py "DEAL WITH IT" --action ok:"▝▔▔▀░▒▓▀▔▀▓▀" --icon ~/.config/dotfiles/deal-with-it/deal-with-it.png -u critical
         else
+            ( killAfter10Min & )
             python3 ~/.config/dotfiles/deal-with-it/multi-actions.py
         fi
         sleep 20
         mplayer /usr/share/sounds/gnome/default/alerts/drip.ogg
         COUNT=0
+        let WALKCOUNT++
     fi
 done
